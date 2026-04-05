95edaf9559f977302b028a801a7d6eba8fde9eeb
Alex1-ai <alexanderemmanuel1719@gmail.com>
2026-04-05 21:35:02 +0100

    bring back the database:

--- app/settings.py
+++ app/settings.py
@@ -121,18 +121,18 @@ AUTH_USER_MODEL = 'accounts.Account'
 
 
 # else:
-DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+# DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
 
 # DATABASE_URL = os.environ.get("DATABASE_URL")
 
-# DATABASES = {
-#     'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)
-# }
+DATABASES = {
+    'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+}
 # DATABASES['default']['OPTIONS'] = {
 #     'connect_timeout': 10,  # Timeout in seconds
 # }
